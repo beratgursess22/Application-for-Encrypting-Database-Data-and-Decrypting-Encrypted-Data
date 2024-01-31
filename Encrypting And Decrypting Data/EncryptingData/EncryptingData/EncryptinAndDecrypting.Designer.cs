2682c1521394010faabf4d7f2d24f9c41c7d11fd
ibrahim berat <149188914+beratgursess22@users.noreply.github.com>
2024-01-31 23:13:20 +0300

    Add files via upload

---
+++ Encrypting And Decrypting Data/EncryptingData/EncryptingData/EncryptinAndDecrypting.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace EncryptingData
+{
+    partial class EncryptinAndDecrypting
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EncryptinAndDecrypting));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtsurname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtmail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtpassoword = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtno = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtacountno = new System.Windows.Forms.TextBox();
+            this.txtpassowrod1 = new System.Windows.Forms.TextBox();
+            this.txtsurname1 = new System.Windows.Forms.TextBox();
+            this.txtmail1 = new System.Windows.Forms.TextBox();
+            this.txtname1 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Info;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-1, 244);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(819, 208);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name:";
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(180, 98);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(154, 34);
+            this.txtname.TabIndex = 2;
+            // 
+            // txtsurname
+            // 
+            this.txtsurname.Location = new System.Drawing.Point(180, 147);
+            this.txtsurname.Name = "txtsurname";
+            this.txtsurname.Size = new System.Drawing.Size(154, 34);
+            this.txtsurname.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Surname:";
+            // 
+            // txtmail
+            // 
+            this.txtmail.Location = new System.Drawing.Point(180, 195);
+            this.txtmail.Name = "txtmail";
+            this.txtmail.Size = new System.Drawing.Size(154, 34);
+            this.txtmail.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(58, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 29);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Mail:";
+            // 
+            // txtpassoword
+            // 
+            this.txtpassoword.Location = new System.Drawing.Point(541, 95);
+            this.txtpassoword.Name = "txtpassoword";
+            this.txtpassoword.Size = new System.Drawing.Size(154, 34);
+            this.txtpassoword.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(400, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(140, 29);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Passoword:";
+            // 
+            // txtno
+            // 
+            this.txtno.Location = new System.Drawing.Point(541, 144);
+            this.txtno.Name = "txtno";
+            this.txtno.Size = new System.Drawing.Size(154, 34);
+            this.txtno.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(400, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(124, 29);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "AcountNo:";
+            // 
+            // btnsave
+            // 
+            this.btnsave.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnsave.Location = new System.Drawing.Point(541, 190);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(154, 34);
+            this.btnsave.TabIndex = 11;
+            this.btnsave.Text = "Save";
+            this.btnsave.UseVisualStyleBackColor = false;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Location = new System.Drawing.Point(214, 265);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 34);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "decrypt ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtacountno);
+            this.groupBox1.Controls.Add(this.txtpassowrod1);
+            this.groupBox1.Controls.Add(this.txtsurname1);
+            this.groupBox1.Controls.Add(this.txtmail1);
+            this.groupBox1.Controls.Add(this.txtname1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Location = new System.Drawing.Point(824, 147);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(378, 299);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            // 
+            // txtacountno
+            // 
+            this.txtacountno.Location = new System.Drawing.Point(176, 218);
+            this.txtacountno.Name = "txtacountno";
+            this.txtacountno.Size = new System.Drawing.Size(192, 34);
+            this.txtacountno.TabIndex = 20;
+            // 
+            // txtpassowrod1
+            // 
+            this.txtpassowrod1.Location = new System.Drawing.Point(176, 166);
+            this.txtpassowrod1.Name = "txtpassowrod1";
+            this.txtpassowrod1.Size = new System.Drawing.Size(192, 34);
+            this.txtpassowrod1.TabIndex = 19;
+            // 
+            // txtsurname1
+            // 
+            this.txtsurname1.Location = new System.Drawing.Point(176, 71);
+            this.txtsurname1.Name = "txtsurname1";
+            this.txtsurname1.Size = new System.Drawing.Size(192, 34);
+            this.txtsurname1.TabIndex = 18;
+            // 
+            // txtmail1
+            // 
+            this.txtmail1.Location = new System.Drawing.Point(176, 117);
+            this.txtmail1.Name = "txtmail1";
+            this.txtmail1.Size = new System.Drawing.Size(192, 34);
+            this.txtmail1.TabIndex = 18;
+            // 
+            // txtname1
+            // 
+            this.txtname1.Location = new System.Drawing.Point(176, 25);
+            this.txtname1.Name = "txtname1";
+            this.txtname1.Size = new System.Drawing.Size(192, 34);
+            this.txtname1.TabIndex = 17;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(9, 223);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(124, 29);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "AcountNo:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(9, 122);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 29);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Mail:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(5, 171);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(140, 29);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "Passoword:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(9, 76);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(116, 29);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Surname:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(9, 25);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(84, 29);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "Name:";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Font = new System.Drawing.Font("Calisto MT", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label16.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label16.Location = new System.Drawing.Point(12, 9);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(779, 56);
+            this.label16.TabIndex = 0;
+            this.label16.Text = "\r\nApplication for Encrypting Database Data and Decrypting Encrypted Data";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(824, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(378, 156);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // EncryptinAndDecrypting
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(1204, 447);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.txtno);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtpassoword);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtmail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtsurname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "EncryptinAndDecrypting";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtsurname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtmail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtpassoword;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtno;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtacountno;
+        private System.Windows.Forms.TextBox txtpassowrod1;
+        private System.Windows.Forms.TextBox txtsurname1;
+        private System.Windows.Forms.TextBox txtmail1;
+        private System.Windows.Forms.TextBox txtname1;
+    }
+}
+
---
+++ Encrypting And Decrypting Data/EncryptingData/EncryptingData/EncryptinAndDecrypting.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace EncryptingData
+{
+    public partial class EncryptinAndDecrypting : Form
+    {
+        public EncryptinAndDecrypting()
+        {
+            InitializeComponent();
+        }
+        SqlConnection connection = new SqlConnection(@"Data Source=LAPTOP-FPTE8E0E\SQLEXPRESS;Initial Catalog=passowordfind;Integrated Security=True");
+
+        void list()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("select * from data", connection);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+        }
+        private void btnsave_Click(object sender, EventArgs e)
+        {
+            string name = txtname.Text;
+            byte[] namearray = ASCIIEncoding.ASCII.GetBytes(name);
+            string namepasso = Convert.ToBase64String(namearray);
+           
+
+            string surname = txtsurname.Text;
+            byte[] surnamearray = ASCIIEncoding.ASCII.GetBytes(surname);
+            string surnamepasso = Convert.ToBase64String(surnamearray);
+
+            string mail = txtmail.Text;
+            byte[] mailarray = ASCIIEncoding.ASCII.GetBytes(mail);
+            string mailpasso = Convert.ToBase64String(mailarray);
+
+            string passoword = txtpassoword.Text;
+            byte[] passowordarray = ASCIIEncoding.ASCII.GetBytes(passoword);
+            string passowrodpasso = Convert.ToBase64String(passowordarray);
+
+            string acountno = txtno.Text;
+            byte[] acountnoarray = ASCIIEncoding.ASCII.GetBytes(acountno);
+            string acountnopasso = Convert.ToBase64String(acountnoarray);
+
+
+            connection.Open();
+            SqlCommand order = new SqlCommand("insert into data (name,surname,mail,pasoword,acountno) values(@p1,@p2,@p3,@p4,@p5)", connection);
+            order.Parameters.AddWithValue("@p1", namepasso);
+            order.Parameters.AddWithValue("@p2", surnamepasso);
+            order.Parameters.AddWithValue("@p3", mailpasso);
+            order.Parameters.AddWithValue("@p4", passowrodpasso);
+            order.Parameters.AddWithValue("@p5", acountnopasso);
+            order.ExecuteNonQuery();
+            connection.Close();
+            MessageBox.Show("Personal is saved", "information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+  
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            list();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            string namefind = txtname1.Text;
+            byte[] namefinarray = Convert.FromBase64String(namefind);
+            string namedata = ASCIIEncoding.ASCII.GetString(namefinarray);
+            txtname1.Text = namedata;
+
+            string surnamefind = txtsurname1.Text;
+            byte[] surnamefindarray = Convert.FromBase64String(surnamefind);
+            string surnamedata = ASCIIEncoding.ASCII.GetString(surnamefindarray);
+            txtsurname1.Text = surnamedata;
+
+            string mailfind = txtmail1.Text;
+            byte[] mailfindarray = Convert.FromBase64String(mailfind);
+            string mailfinddata = ASCIIEncoding.ASCII.GetString(mailfindarray);
+            txtmail1.Text = mailfinddata;
+           
+
+            string passowrodfind = txtpassowrod1.Text;
+            byte[] passowrodfindarray = Convert.FromBase64String(passowrodfind);
+            string passowordfindata = ASCIIEncoding.ASCII.GetString(passowrodfindarray);
+            txtpassowrod1.Text = passowordfindata;
+
+            string acountnofind = txtacountno.Text;
+            byte[] acountnofindarray = Convert.FromBase64String(acountnofind);
+            string acounnofinddata = ASCIIEncoding.ASCII.GetString(acountnofindarray);
+            txtacountno.Text = acounnofinddata;
+            
+            
+        }
+        
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int chosen = dataGridView1.SelectedCells[0].RowIndex;
+            txtname1.Text = dataGridView1.Rows[chosen].Cells[1].Value.ToString();
+            txtsurname1.Text = dataGridView1.Rows[chosen].Cells[2].Value.ToString();
+            txtmail1.Text = dataGridView1.Rows[chosen].Cells[3].Value.ToString();
+            txtpassowrod1.Text = dataGridView1.Rows[chosen].Cells[4].Value.ToString();
+            txtacountno.Text = dataGridView1.Rows[chosen].Cells[5].Value.ToString();
+        }
+    }
+}
